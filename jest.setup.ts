8fbd0da118ca7bf623ce4fb575f302dd50933f3c
imotai <codego.me@gmail.com>
2023-02-09 19:36:30 +0800

    fix: add global config to setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,10 @@
 import { TextEncoder, TextDecoder } from 'util'
 globalThis.TextEncoder = TextEncoder
 globalThis.TextDecoder = TextDecoder
+import fetch, { Headers, Request, Response } from 'node-fetch'
+if (!globalThis.fetch) {
+    globalThis.fetch = fetch
+    globalThis.Headers = Headers
+    globalThis.Request = Request
+    globalThis.Response = Response
+}
