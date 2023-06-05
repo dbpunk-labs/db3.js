726358b8bd67e46316ea379cb4917a43ecef6b17
imotai <codego.me@gmail.com>
2023-06-05 22:01:32 +0800

    feat: format style

--- src/store/app.ts
+++ src/store/app.ts
@@ -14,7 +14,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// @ts-nocheck
 
 import { DB3Client } from '../client/client'
 import { Wallet } from '../wallet/wallet'
--- src/store/collection.ts
+++ src/store/collection.ts
@@ -14,7 +14,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// @ts-nocheck
 
 import { Index } from '../proto/db3_database'
 import { DB3Store } from './database'
